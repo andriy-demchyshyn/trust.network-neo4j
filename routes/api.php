1eef65b0b542309e9ce1d48dcc7052711ad63fb3
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-01 16:00:03 +0300

    Updated api routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\TrustConnectionController;
 use App\Http\Controllers\MessageController;
 
 Route::apiResources([
     'people' => PeopleController::class,
+    'people.trust_connections' => TrustConnectionController::class,
     'messages' => MessageController::class,
 ]);
