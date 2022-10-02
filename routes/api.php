03d8513d9db5d21e4385979c81468cef59bbcdc1
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-02 14:44:50 +0300

    Updated api routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeopleController;
 use App\Http\Controllers\TrustConnectionController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\PathController;
 
-Route::apiResources([
-    'people' => PeopleController::class,
-    'people.trust_connections' => TrustConnectionController::class,
-    'messages' => MessageController::class,
-]);
+Route::post('/people', [PeopleController::class, 'store']);
+Route::post('/people/{person_id}/trust_connections', [TrustConnectionController::class, 'store']);
+Route::post('/messages', [MessageController::class, 'store']);
+Route::post('/path', [PathController::class, 'store']);
