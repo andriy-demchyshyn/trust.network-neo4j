d98007951cf1aaeef2a8b0d1aa30d424a78764c0
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-03 00:05:02 +0300

    Updated Message Controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -31,40 +31,42 @@ class MessageController extends Controller
      */
     public function store(StoreMessageRequest $request)
     {
-        $query = $this->neo4j->run(<<<'CYPHER'
-        MATCH (author:People {id: $from_person_id})
-        CREATE (message:Messages {
-            text: $text,
-            topics: $topics,
-            from_person_id: $from_person_id,
-            min_trust_level: $min_trust_level,
-            created_at: $created_at
-        })
-        CREATE (author)-[:CAN_VIEW]->(message)
-        WITH message
-        MATCH path = (author:People {id: $from_person_id}) - [:TRUSTS*] -> (receiver:People)
-        WHERE
-            receiver.id <> $from_person_id AND
-            all(trust IN relationships(path) WHERE trust.level >= $min_trust_level) AND
-            all(node IN nodes(path) WHERE all(topic IN $topics WHERE topic IN node.topics))
-        WITH distinct receiver, message
-        CREATE (receiver)-[:CAN_VIEW]->(message)
-        return receiver.id as receiver_id
-        CYPHER, [
-            'text' => $request->safe()->text,
-            'topics' => $request->safe()->topics,
-            'from_person_id' => $request->safe()->from_person_id,
-            'min_trust_level' => $request->safe()->min_trust_level,
-            'created_at' => time()
-        ]);
+        try {
+            $query = $this->neo4j->run(<<<'CYPHER'
+            MATCH (author:People {id: $from_person_id})
+            CREATE (message:Messages {
+                text: $text,
+                topics: $topics,
+                from_person_id: $from_person_id,
+                min_trust_level: $min_trust_level
+            })
+            CREATE (author)-[:CAN_VIEW]->(message)
+            WITH message
+            MATCH path = (author:People {id: $from_person_id}) - [:TRUSTS*] -> (receiver:People)
+            WHERE
+                receiver.id <> $from_person_id AND
+                all(trust IN relationships(path) WHERE trust.level >= $min_trust_level) AND
+                all(node IN nodes(path) WHERE all(topic IN $topics WHERE topic IN node.topics))
+            WITH distinct receiver, message
+            CREATE (receiver)-[:CAN_VIEW]->(message)
+            return receiver.id as receiver_id
+            CYPHER, [
+                'text' => $request->safe()->text,
+                'topics' => $request->safe()->topics,
+                'from_person_id' => $request->safe()->from_person_id,
+                'min_trust_level' => $request->safe()->min_trust_level
+            ]);
 
-        $results = [];
-        foreach ($query as $item) {
-            $results[] = $item->get('receiver_id');
+            $results = [];
+            foreach ($query as $item) {
+                $results[] = $item->get('receiver_id');
+            }
+
+            return !empty($results) 
+                ? response()->json([$request->safe()->from_person_id => $results], 201) 
+                : response('Not found', 404);
+        } catch (\Exception $e) {
+            abort(422, $e->getMessage());
         }
-
-        return response()->json([
-            $request->safe()->from_person_id => $results,
-        ], 201);
     }
 }
