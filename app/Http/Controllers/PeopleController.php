383a5b5daa3c81a7ddae4ea16d2685783ca4f323
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-02 14:47:17 +0300

    Updated People Controller

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\People\ShowPeopleRequest;
-use App\Http\Requests\People\ShowPersonRequest;
 use App\Http\Requests\People\StorePersonRequest;
-use App\Http\Requests\People\UpdatePersonRequest;
-use App\Http\Requests\People\DestroyPersonRequest;
 use App\Services\Neo4jClient;
 
 class PeopleController extends Controller
@@ -28,33 +24,6 @@ class PeopleController extends Controller
     }
 
     /**
-     * Show people
-     * 
-     * @param  \App\Http\Requests\People\ShowPeopleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function index(ShowPeopleRequest $request)
-    {
-        abort(403); // Method is not used yet, should be implemented in future
-    }
-
-    /**
-     * Show person
-     * 
-     * @param  \App\Http\Requests\People\ShowPersonRequest  $request
-     * @param  string  $person_id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ShowPersonRequest $request, string $person_id)
-    {
-        $query = $this->neo4j->run(<<<'CYPHER'
-        MATCH (person:People {id: $id}) RETURN person
-        CYPHER, ['id' => $person_id]);
-
-        return response()->json($query->first()->get('person')->getProperties(), 200);
-    }
-
-    /**
      * Store new person
      * 
      * @param  \App\Http\Requests\People\StorePersonRequest  $request
@@ -72,29 +41,5 @@ class PeopleController extends Controller
         CYPHER, ['id' => $request->safe()->id, 'topics' => $request->safe()->topics]);
 
         return response()->json($query->first()->get('person')->getProperties(), 201);
-    }
-
-    /**
-     * Update person
-     * 
-     * @param  \App\Http\Requests\People\UpdatePersonRequest  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePersonRequest $request, $id)
-    {
-        abort(403); // Method is not used yet, should be implemented in future
-    }
-
-    /**
-     * Remove person
-     * 
-     * @param  \App\Http\Requests\People\DestroyPersonRequest  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(DestroyPersonRequest $request, $id)
-    {
-        abort(403); // Method is not used yet, should be implemented in future
     }
 }
