ae6de2c77005e90475a9dc5f51f24d29c6a9b3dc
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-03 17:21:31 +0300

    Updated Path Controller

--- app/Http/Controllers/PathController.php
+++ app/Http/Controllers/PathController.php
@@ -39,8 +39,10 @@ class PathController extends Controller
                 receiver.id <> $from_person_id AND
                 all(trust IN relationships(path) WHERE trust.level >= $min_trust_level) AND
                 all(topic IN $topics WHERE topic IN receiver.topics)
-            WITH distinct receiver, path
-            return receiver.id as receiver_id, nodes(path) as nodes, length(path) as path_length
+            WITH distinct receiver, nodes(path) as nodes, length(path) as path_length
+            ORDER BY path_length ASC
+            LIMIT 1
+            RETURN nodes
             CYPHER, [
                 'text' => $request->safe()->text,
                 'topics' => $request->safe()->topics,
@@ -51,39 +53,19 @@ class PathController extends Controller
             abort(422, 'Unprocessable request data');
         }
 
-        $shortest_path = $this->selectShortestPath($request, $query);
+        if (empty($query) || empty($query->getResults()->jsonSerialize())) {
+            abort(404, 'Path not found');
+        }
 
-        return !empty($shortest_path) 
-            ? response()->json(['from' => $request->safe()->from_person_id, 'path' => $shortest_path], 201) 
-            : abort(404, 'Path not found');
-    }
-
-    /**
-     * Select shortest path
-     * 
-     * @param  \Illuminate\Http\Request  $request
-     * @param  mixed  $query
-     * @return array
-     */
-    protected function selectShortestPath(Request $request, mixed $query): array
-    {
-        $shortest_path = [];
-        foreach ($query->getResults() as $item) {
-            $path_length = $item->get('path_length');
-            if (!empty($shortest_path) && $path_length >= count($shortest_path)) {
-                continue;
-            }
-
-            $shortest_path = [];
-            $path_nodes = $item->get('nodes');
-            foreach ($path_nodes as $path_node) {
-                if ($path_node->getProperty('id') === $request->safe()->from_person_id) {
-                    continue;
-                }
-                $shortest_path[] = $path_node->getProperty('id');
+        $path = [];
+        foreach ($query->first()->get('nodes') as $node) {
+            if ($node->getProperty('id') != $request->safe()->from_person_id) {
+                $path[] = $node->getProperty('id');
             }
         }
 
-        return $shortest_path;
+        return !empty($path) 
+            ? response()->json(['from' => $request->safe()->from_person_id, 'path' => $path], 201) 
+            : abort(404, 'Path not found');
     }
 }
