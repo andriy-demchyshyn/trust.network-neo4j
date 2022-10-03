bccab017dccedc9b36d24cfddcecd9f8a315a8fe
Andriy Demchyshyn <andrii.demchyshyn@gmail.com>
2022-10-03 04:24:35 +0300

    Updated api routes

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,4 @@ use App\Http\Controllers\PathController;
 Route::post('/people', [PeopleController::class, 'store']);
 Route::post('/people/{person_id}/trust_connections', [TrustConnectionController::class, 'store']);
 Route::post('/messages', [MessageController::class, 'store']);
-Route::post('/path', [PathController::class, 'store']);
+Route::post('/path', [PathController::class, 'findShortestPath']);
